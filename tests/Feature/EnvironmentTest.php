00fd5dce3f39995c7a5f7c9ae6ba04cf396726de
shandikadav <shandikadavid.14@gmail.com>
2025-01-08 17:28:33 +0700

    feat: completing env module

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    public function testGetEnv() {
+        $youtube = env('YOUTUBE');
+
+        self::assertEquals('Programmer Zaman Now', $youtube);
+    }
+
+    public function testDefaultEnv() {
+        $author = env('AUTHOR', 'Shandika');
+
+        self::assertEquals('Shandika', $author);
+    }
+}
