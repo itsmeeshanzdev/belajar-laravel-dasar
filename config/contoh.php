c217ec58ba09f8d4c26f35140c7419727c2daa1e
shandikadav <shandikadavid.14@gmail.com>
2025-01-08 17:46:29 +0700

    feat: completing configuration module

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => [
+        "first" => env('NAME_FIRST', "Shandika"),
+        "last" => env('NAME_LAST', "Ardiansyah")
+    ],
+    "email" => "shandikadavid.14@gmail.com",
+    "web" => "https://shandikadav.my.id"
+];
